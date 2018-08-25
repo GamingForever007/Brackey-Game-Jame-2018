2d5f6fa55bac5d5540ae8d629fde7ef1405a6e66
Aleksander Kovač <aleksander.kovac97@hotmail.com>
2018-08-25 10:15:26 +0200

    Instantiated the project
    Instantiated the project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿//A script that moves the player
+//Author: Aleksander Kovač & S4NNY1
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody rigidbody;
+
+    //variables that are goint to be updated 
+    [SerializeField]
+    private int maxJumps = 1;
+
+    [SerializeField]
+    private int jumpForce = 200;
+
+	void Start () {
+
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space) && maxJumps > 0)
+        {
+            maxJumps--;
+            rigidbody.velocity = new Vector3(0, jumpForce * Time.deltaTime, 0);
+        }
+	}
+}
